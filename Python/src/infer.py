2b182a2d119b3db8d44cef527274581cfdcdfab2
Robin Courant <courant.robin@hotmail.fr>
2021-12-22 17:35:33 +0100

    Add a training pl pipeline

--- Python/src/infer.py
+++ Python/src/infer.py
@@ -1,27 +1,42 @@
 from mlagents_envs.environment import UnityEnvironment
 from omegaconf import DictConfig
+from pytorch_lightning import Trainer
 
-from src.models.trainer import Trainer
-from src.models.modules.DQN import DQN
+from src.models.dqn_module import DQNModule
 
 
 def infer(config: DictConfig):
+    """TODO"""
     env = UnityEnvironment(config.env.path, no_graphics=not config.env.display)
     env.reset()
-
     behavior_name = list(env.behavior_specs)[0]
-    model = DQN(config.model)
-    trainer = Trainer(
-        model=model,
+
+    model = DQNModule(
+        batch_size=config.compnode.batch_size,
+        lr=config.model.learning_rate,
         env=env,
-        greedy_ratio=config.model.greedy_ratio,
-        max_episode_steps=config.model.max_episode_steps,
-        max_succeded_episodes=config.model.max_succeded_episodes,
-        update_frequency=config.model.update_frequency,
-        project_name=config.project_name,
-        xp_name=config.xp_name,
-        log_dir=config.log_dir,
+        behavior_name=behavior_name,
+        n_actions=config.env.num_actions,
+        gamma=config.model.gamma,
+        sync_rate=config.model.sync_rate,
+        replay_size=config.model.replay_size,
+        warm_start_size=config.model.warm_start_size,
+        eps_last_frame=config.model.eps_last_frame,
+        eps_start=config.model.eps_start,
+        eps_end=config.model.eps_end,
+        episode_length=config.model.episode_length,
+        warm_start_steps=config.model.warm_start_steps,
+        lr_reduce_rate=config.model.lr_reduce_rate,
+        weight_decay=config.model.weight_decay,
+        run_type=config.run_type,
     )
-    trainer.infer(behavior_name, config.model.n_episodes)
+
+    trainer = Trainer(
+        gpus=config.compnode.num_gpus,
+        num_nodes=config.compnode.num_nodes,
+        accelerator=config.compnode.accelerator,
+        max_epochs=config.model.n_episodes,
+    )
+    trainer.test(model, ckpt_path=config.model.checkpoint_path)
 
     env.close()
